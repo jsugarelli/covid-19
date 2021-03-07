d6a136b573105fc33e73d0f5c54251f89777186b
Joachim Zuckarelli <joachim@zuckarelli.de>
2021-03-07 16:18:44 +0100

    Vaccination data added

--- README.md
+++ README.md
@@ -1,31 +1,28 @@
-Covid-19 in One Graph
+Covid-19 Situation in Germany
 ================
 
-### Current Situation in Germany
-
-The graph shows ICU capacity usage for adult ICU beds in percent of
-total capacity as well as the 7-day incidence and a 7-day moving average of Covid-19-related death cases. Last reported data is from **March 4th**.
+### Current Situation in One Graph
+Last reported data is from **March 4th**.
 
 ![](onepicture.png)
 
-The vertical blue lines mark the coming into effect of two major
-government measures: a partial lockdown on November 2nd (called
-*lockdown light* in Germany; essentially restrictions on personal
-contacts, closing of restaurants/cafés/bars, except for takeaway, ban on
-public events, closing of leisure and sports facilities as well as all
-body care related services, except for necessary medical care), and a
-tighter lockdown (called *hard lockdown* in Germany; lockdown light plus
-closing of all shops except for essential supplies and services, closing
-of schools).
+
+### Explanations
+
+The vertical dark red lines mark the coming into effect of two major government measures: a partial lockdown on November 2nd (called **lockdown light** in Germany; essentially restrictions on personal contacts, closing of restaurants/cafés/bars, except for takeaway, ban on public events, closing of leisure and sports facilities as well as all body care related services, except for necessary medical care), and a tighter lockdown (called **hard lockdown** in Germany; lockdown light plus closing of all shops except for essential supplies and services, closing of schools).
+
+
+With effect from March 1st (vertical green line), the German government has decided to take first steps towards a **cautious re-opening** of the economy and the education sector. From March 1st onwards hairdressers may open their shops again; the same applies to schools and child care institutions, depending on decisions on the federal state level. Later during week of March 1st the government decided to allow further re-openings according to a plan stipulating five opening phases the first of which is considered to be the March 1st opening of hairdressers and schools. Phases according to the plan will be take effect locally on the county level, depending on the 7-day-incidence rate of the county. Details are available from the [federal government](https://www.bundesregierung.de/breg-de/themen/coronavirus/fuenf-oeffnungsschritte-1872120)
 
 
 ### Data sources & code
 
-ICU usage data is from the German DIVI intensive care register operated by the
-[Robert-Koch-Institut]() (RKI) and the [Deutsche Interdisziplinäre
-Vereinigung für Intensiv- und Notfallmedizin](https://www.divi.de/)
-(DIVI), and can be obtained from
-<https://www.intensivregister.de/#/aktuelle-lage/reports> (click "Zeitreihen-Daten" to download the time series data).
-Death cases and 7-day incidence data is taken from the [Robert-Koch-Institut](https://www.rki.de/DE/Content/InfAZ/N/Neuartiges_Coronavirus/Daten/Fallzahlen_Daten.html;jsessionid=FB9800F61AE81ACFF850FB2567F1F1DB.internet071?nn=2386228).
+* **ICU usage data**:  DIVI intensive care register operated by the [Robert-Koch-Institut]() (RKI) and the [Deutsche Interdisziplinäre
+Vereinigung für Intensiv- und Notfallmedizin](https://www.divi.de/); data can be downloaded from <https://www.intensivregister.de/#/aktuelle-lage/reports> (click "Zeitreihen-Daten" to download the time series data).
+* **Death cases** and **7-day incidence data**: [Robert-Koch-Institut](https://www.rki.de/DE/Content/InfAZ/N/Neuartiges_Coronavirus/Daten/Fallzahlen_Daten.html;jsessionid=FB9800F61AE81ACFF850FB2567F1F1DB.internet071?nn=2386228).
+* **Vaccination rate** and **vaccination dose availability data** for **Germany**: [Vaccination dashboard of the German Government](https://impfdashboard.de/daten)
+* **Vaccination rate data** for the **United States**: [Our World in Data](https://ourworldindata.org/covid-vaccinations?country=~USA)
+* **Dates of market availability of vaccines in Germany**: [Robert-Koch-Institut](https://www.rki.de/DE/Content/InfAZ/N/Neuartiges_Coronavirus/Daten/Impfquotenmonitoring.xlsx?__blob=publicationFile)
+* **Population number Germany**: [Federal Statistical Office of Germany](https://www.destatis.de/DE/Themen/Gesellschaft-Umwelt/Bevoelkerung/Bevoelkerungsstand/Tabellen/zensus-geschlecht-staatsangehoerigkeit-2020.html)
 
 All data (already pre-prepared) and R source code can be found in this repository.
--- covidgraph.r
+++ covidgraph.r
@@ -1,10 +1,14 @@
 library(ggplot2)
+library(ggrepel)
 library(reshape2)
 library(scales)
 library(tidyverse)
 library(lubridate)
 library(stringr)
 
+
+# TODO: Update Readme
+# TODO: Axes in color, legend entries in same colors
 
 
 # --------------- Support functions --------------- 
@@ -23,20 +27,23 @@ ma <- function(x, order = 7) {
 
 # --------------- Data on ICU capacity and its usage --------------- 
 
-divi <- read.csv("icu_usage.csv", header = TRUE, sep =";")
+divi <- as_tibble(read.csv("icu_usage.csv", header = TRUE, sep =";"))
 divi <- divi[divi$Bundesland == "DEUTSCHLAND",]
 divi <- divi[, c(1,4,5,6)]
 names(divi) <- c("date", "covid", "used", "free")
-
 divi$date <- ymd(substr(divi$date, 1, 10))
+
 divi$capacity <- divi$used + divi$free
 divi$usedp <- (divi$used / divi$capacity) * 100
-divi$freep <- 100 - divi$usedp
 divi$covidp <- (divi$covid / divi$capacity) * 100
+
+divi$usedp <- ma(divi$usedp)
+divi$covidp <- ma(divi$covidp)
+
+divi$freep <- 100 - divi$usedp
 divi$noncovidp <- divi$usedp - divi$covidp
 
 divi <- divi[divi$date >= ymd("2020-10-01"), ]
-
 divi <- divi[,c ("date", "freep", "noncovidp", "covidp")]
 
 
@@ -61,10 +68,47 @@ class(inc$incidence) <- "numeric"
 
 
 
+# --------------- Data on vaccinations --------------- 
+
+# Germany
+vacc_de <- as_tibble(read.csv("vaccinations_de.csv", sep = ";", header = TRUE, stringsAsFactors = FALSE))
+population <- 83190556
+vacc_de$doses <- (vacc_de$dosen_biontech_kumulativ + vacc_de$dosen_moderna_kumulativ + vacc_de$dosen_astrazeneca_kumulativ)
+vacc_de$dosesp <- (vacc_de$doses/2)/population * 100
+vacc_de <- vacc_de[,c("date", 
+                      "personen_erst_kumulativ", 
+                      "personen_voll_kumulativ",
+                      "impf_quote_erst", 
+                      "impf_quote_voll", 
+                      "doses", 
+                      "dosesp")]
+names(vacc_de) <- c("date", 
+                    "firstvacc_de", 
+                    "secvacc_de", 
+                    "firstvaccrate_de", 
+                    "secvaccrate_de", 
+                    "doses", 
+                    "dosesp")
+vacc_de$firstvaccrate_de <- vacc_de$firstvaccrate_de * 100
+vacc_de$secvaccrate_de <- vacc_de$secvaccrate_de * 100
+vacc_de$date <- dmy(vacc_de$date) 
+
+# United States
+vacc_us <- as_tibble(read.csv("vaccinations_us.csv", sep = ",", header = TRUE, stringsAsFactors = FALSE))
+names(vacc_us) <- c("country", "code", "date", "secvaccrate_us")
+vacc_us$date <- ymd(vacc_us$date)
+vacc_us$firstvaccrate_us <- vacc_us$firstvaccrate_us
+vacc_us <- vacc_us[vacc_us$country == "United States",]
+vacc_us <- vacc_us[, c(3:NCOL(vacc_us))]
+
+
+
 # --------------- Merge and melt dataset --------------- 
 
 full <- inner_join(divi, deaths, by =c("date"))
 full <- inner_join(full, inc, by =c("date"))
+full <- left_join(full, vacc_de, by =c("date"))
+full <- left_join(full, vacc_us, by =c("date"))
 full.long <- melt(full, id.vars = "date", value.name = "val")
 
 
@@ -73,17 +117,105 @@ full.long <- melt(full, id.vars = "date", value.name = "val")
 
 coeff1 <- 12
 graph <- ggplot() +
-  geom_bar(data = subset(full.long, variable %in% c("freep", "covidp", "noncovidp")), aes(x = date, y = val, fill = variable), stat = "identity", width = 1, alpha = 0.75) +
-  geom_line(data = subset(full.long, variable %in% c("newdeaths")), aes(x = date, y = val/coeff1, color = "red3"), size = 1.1) +
-  geom_line(data = subset(full.long, variable %in% c("incidence")), aes(x = date, y = val/coeff1, color = "darkorange2"), size = 1.1) +
-  geom_vline(xintercept = ymd("2020-12-16"), linetype="solid", color = "blue4", size=1) +
-  geom_vline(xintercept = ymd("2020-11-02"), linetype="solid", color = "blue4", size=1) +
-  geom_label(aes(x = ymd("2020-11-02"), y = 85, label = "Lockdown \"light\""), fill = "white", color = "blue4") +
-  geom_label(aes(x = ymd("2020-12-16"), y = 95, label = "Lockdown \"hard\""), fill = "white", color = "blue4") +
-  scale_fill_manual(values = c("gray68", "steelblue2", "dodgerblue4"), labels = c("Free capacity","Used capacity (non-Covid-19)","Used capacity (Covid-19)")) +
-  scale_color_manual(values = c("darkorange2", "red2"), labels = c("7-day incidence", "Death cases (7-day rolling average)")) +
+  geom_bar(data = subset(full.long, variable %in% c("freep", "covidp", "noncovidp")), 
+           aes(x = date, y = val, fill = variable), stat = "identity", width = 1, 
+           alpha = 0.75) +
+  geom_bar(data = subset(full.long, variable %in% c("dosesp")), 
+           aes(x = date, y = val, fill = variable), stat = "identity", width = 1, 
+           alpha = 0.50) +
+  geom_line(data = subset(full.long, variable %in% c("incidence")), 
+            aes(x = date, y = val/coeff1, color = "col1"), size = 1.1) +
+  geom_line(data = subset(full.long, variable %in% c("newdeaths")), 
+            aes(x = date, y = val/coeff1, color = "col2"), size = 1.1) +
+  geom_line(data = subset(full.long, variable %in% c("secvaccrate_de")),
+            aes(x = date, y = val, color = "col3"), size = 1.1) +
+  geom_line(data = subset(full.long, variable %in% c("secvaccrate_us")), 
+            aes(x = date, y = val, color = "col4"), size = 1.1) +
+  geom_vline(xintercept = ymd("2020-12-16"), linetype="solid", color = "red4", 
+             size=0.7) +
+  geom_vline(xintercept = ymd("2020-11-02"), linetype="solid", color = "red4", 
+             size=0.7) +
+  geom_vline(xintercept = ymd("2021-03-01"), linetype="solid", color = "darkgreen", 
+             size=0.7) +
+  geom_vline(xintercept = ymd("2020-12-26"), linetype="solid", color = "royalblue4", 
+             size=0.7) +
+  geom_vline(xintercept = ymd("2021-01-14"), linetype="solid", color = "royalblue4", 
+             size=0.7) +
+  geom_vline(xintercept = ymd("2021-02-08"), linetype="solid", color = "royalblue4", 
+             size=0.7) +
+  geom_label(aes(x = ymd("2020-11-02"), y = 85, label = "Lockdown \"light\""), 
+             fill = "white", color = "red4") +
+  geom_label(aes(x = ymd("2020-12-16"), y = 95, label = "Lockdown \"hard\""), 
+             fill = "white", color = "red4") +
+  geom_label(aes(x = ymd("2021-03-01"), y = 85, label = "Start of cautious\nre-openings"), 
+             fill = "white", color = "darkgreen") +
+  geom_label(aes(x = ymd("2020-12-26"), y = 85, label = "BioNTech vacc.\navailable"), 
+             fill = "white", color = "royalblue4") +
+  geom_label(aes(x = ymd("2021-01-14"), y = 85, label = "Moderna vacc.\navailable"), 
+             fill = "white", color = "royalblue4") +
+  geom_label(aes(x = ymd("2021-02-08"), y = 85, label = "AstraZeneca vacc.\navailable"), 
+             fill = "white", color = "royalblue4") +
+  geom_text_repel(aes(label = round(val, 1), x = date, y = val/coeff1), 
+                  data = full.long[full.long$date == max(full.long$date) & full.long$variable == "incidence",], 
+                  size = 3.5, fontface = "bold", color = "darkorange2", nudge_x = 1, 
+                  nudge_y = 1, segment.colour = "transparent") +
+  geom_text_repel(aes(label = round(val, 1), x = date, y = val/coeff1), 
+                  data = full.long[full.long$date == max(full.long$date) & full.long$variable == "newdeaths",], 
+                  size = 3.5, fontface = "bold", color = "red2", nudge_x = 1, 
+                  segment.colour = "transparent") +
+  geom_text_repel(aes(label = round(val, 1), x = date, y = val), 
+                  data = full.long[full.long$date == max(full.long$date) & full.long$variable == "secvaccrate_de",], 
+                  size = 3.5, fontface = "bold", color = "springgreen3", nudge_x = 1,
+                  nudge_y = -1, segment.colour = "transparent") +
+  geom_text_repel(aes(label = round(val, 1), x = date, y = val), 
+                  data = full.long[full.long$date == max(full.long$date) & full.long$variable == "secvaccrate_us",], 
+                  size = 3.5, fontface = "bold", color = "gray52", nudge_x = 1, 
+                  nudge_y = 1, segment.colour = "transparent") +
+  geom_text_repel(aes(label = round(val, 1), x = date, y = val), 
+                  data = full.long[full.long$date == max(full.long$date) & full.long$variable == "covidp",], 
+                  size = 3.5, fontface = "bold", color = "dodgerblue3", nudge_x = 1, 
+                  segment.colour = "transparent") +
+  geom_text_repel(aes(label = round(val, 1), x = date, y = val), 
+                  data = full.long[full.long$date == max(full.long$date) & full.long$variable == "noncovidp",], 
+                  size = 3.5, fontface = "bold", color = "steelblue2", nudge_x = 1, 
+                  segment.colour = "transparent") +
+  geom_text_repel(aes(label = round(val, 1), x = date, y = val), 
+                  data = full.long[full.long$date == max(full.long$date) & full.long$variable == "freep",], 
+                  size = 3.5, fontface = "bold", color = "gray68", nudge_x = 2, nudge_y = 80, 
+                  segment.colour = "transparent") +
+  geom_text_repel(aes(label = round(val, 1), x = date, y = val), 
+                  data = full.long[full.long$date == max(full.long$date) & full.long$variable == "dosesp",], 
+                  size = 3.5, fontface = "bold", color = "chartreuse3", nudge_x = 1, nudge_y = 0, 
+                  segment.colour = "transparent") +
+  scale_fill_manual(values = c("freep" = "gray68", 
+                               "noncovidp" = "steelblue2", 
+                               "covidp" = "dodgerblue3", 
+                               "dosesp" = "chartreuse3"), 
+                    labels = c("freep" = "Free ICU capacity, 7-day roll. avg.",
+                               "noncovidp" = "Used ICU capacity (non-Covid-19), 7-day roll. avg.",
+                               "covidp" = "Used ICU capacity (Covid-19), 7-day roll. avg.", 
+                               "dosesp" = "Avail. of doses for full vacc. (% of pop.)")) +
+  scale_color_manual(values = c(col1 = "darkorange2", 
+                                col2 = "red2", 
+                                col3 = "springgreen3", 
+                                col4 = "gray52"), 
+                     labels = c("7-day incidence",
+                                "Death cases (7-day roll. avg.)", 
+                                "Vaccination rate (full) Germany", 
+                                "Vaccination rate (full) US")) +
   scale_y_continuous(breaks = seq(0, 100, 5),
-              sec.axis = sec_axis(~.*coeff1, name = "Death cases / 7-day incidence")) +
-  scale_x_date(date_breaks = waiver(), date_minor_breaks = "1 week", limits = c(min(full.long$date), max(full.long$date))) +
-  labs(title = "Covid-19 Situation in Germany", x = "", y = "Percent of total ICU capacity", fill = "Left scale:", color = "Right scale:") +
-  theme(text = element_text(size = 11, face = "bold"), legend.text = element_text(size = 11, colour = "grey33", face = "plain"), legend.position = "bottom", legend.key.size = unit(12, "points"), legend.background = element_rect(fill = "gray94"), axis.title = element_text(size = 13, face = "plain"))
+              sec.axis = sec_axis(~.*coeff1, name = "Death cases / 7-day incidence", 
+                                  breaks = seq(0, 1250, by = 50))) +
+  scale_x_date(date_breaks = waiver(), date_minor_breaks = "1 week", 
+               limits = c(min(full.long$date), max(full.long$date))) +
+  labs(title = "", 
+       x = "", y = "Percent of total ICU capacity / Vaccination rates", 
+       fill = "Fill", color = "Lines") +
+  theme(text = element_text(size = 11, face = "bold"), 
+        legend.text = element_text(size = 11, colour = "grey33", face = "plain"),
+        legend.position = "bottom",
+        legend.key.size = unit(12, "points"), 
+        legend.background = element_rect(fill = "gray94"), 
+        axis.title = element_text(size = 13, face = "plain")) +
+  guides(fill = guide_legend(nrow = 2, byrow = TRUE), 
+         color = guide_legend(nrow = 2, byrow = TRUE))
